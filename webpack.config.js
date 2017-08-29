77aaddb7e2aacb23036bb10ed6f1a65e613ea1c5
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-08-29 11:47:42 -0700

    Inital commit. Configure webpack and package.json

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-inline-source-map',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle_app.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: 'babel-loader' },
+      { test: /\.css$/, loaders: 'style-loader!css-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ]
+};
