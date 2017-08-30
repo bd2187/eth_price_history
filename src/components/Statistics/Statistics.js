0e381834f891eadfaa65807e6f0e92e855b596f0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-08-30 11:17:56 -0700

    Add proptypes to Statistics component. Make contents of Statistics.js more modular by creating StatsBox component which will display stats in Statistics component

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,31 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { today, week, month, year } from '../../config/categories';
 import './statistics.css';
-
-function renderLabel(category, type) {
-  switch (category) {
-    case today:
-      return `24 hour ${type}`;
-    case week:
-      return `1 week ${type}`;
-    case month:
-      return `1 month ${type}`;
-    case year:
-      return `1 year ${type}`;
-    default:
-      return null;
-  }
-}
 
 function sortUSD(arr) {
   return arr.map(item => item.usd).sort((prev, next) => prev - next);
 }
 
+// StatsBox renders in Statistics Component
+class StatsBox extends Component {
+  renderLabel(category, type) {
+    switch (category) {
+      case today:
+        return `24 hour ${type}`;
+      case week:
+        return `1 week ${type}`;
+      case month:
+        return `1 month ${type}`;
+      case year:
+        return `1 year ${type}`;
+      default:
+        return null;
+    }
+  }
+  render() {
+    const { price, category, type } = this.props;
+    return (
+      <div className="statsBox">
+        <h2 className="label">
+          {this.renderLabel(category, type)}
+        </h2>
+        <h2 className="amount">
+          {`$${price}`}
+        </h2>
+      </div>
+    );
+  }
+}
+
+StatsBox.propTypes = {
+  price: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
+};
+
 function Statistics({ modifiedData, category, currentETHWorth }) {
-  const sortedUSDArr = sortUSD(modifiedData);
-  console.log(sortedUSDArr);
+  const sortedUSDArr = sortUSD(modifiedData); // returns array of USD in ascending order
   return (
     <div className="statistics">
+      {/*Display current ETH worth in USD*/}
       <div className="statsBox" id="current-eth-usd">
         <p>
           {'1 ETH = '}
@@ -34,26 +57,22 @@ function Statistics({ modifiedData, category, currentETHWorth }) {
       </div>
 
       <div className="mediaQuery">
-        <div className="statsBox">
-          <h2 className="label">
-            {renderLabel(category, 'low')}
-          </h2>
-          <h2 className="amount">
-            {`$${sortedUSDArr[0]}`}
-          </h2>
-        </div>
-
-        <div className="statsBox">
-          <h2 className="label">
-            {renderLabel(category, 'high')}
-          </h2>
-          <h2 className="amount">
-            {`$${sortedUSDArr[sortedUSDArr.length - 1]}`}
-          </h2>
-        </div>
+        {/*Display lowest and highest ETH worth in USD in desired category/timeframe*/}
+        <StatsBox price={sortedUSDArr[0]} category={category} type={'low'} />
+        <StatsBox
+          price={sortedUSDArr[sortedUSDArr.length - 1]}
+          category={category}
+          type={'high'}
+        />
       </div>
     </div>
   );
 }
+
+Statistics.propTypes = {
+  modifiedData: PropTypes.array.isRequired,
+  category: PropTypes.string.isRequired,
+  currentETHWorth: PropTypes.object.isRequired
+};
 
 export default Statistics;
