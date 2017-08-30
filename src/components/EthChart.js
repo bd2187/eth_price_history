010a8a9b3ee8b2e7713c98db13179a9aaa4a5a8e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-08-29 18:52:10 -0700

    Add convertDate function to EthChart.js

--- src/components/EthChart.js
+++ src/components/EthChart.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 import { today, week, month, year } from '../config/categories';
 
+function convertDate(time) {
+  var date = new Date(time);
+  var hour = date.getHours();
+  var minutes = date.getMinutes();
+  return `${date.getMonth() +
+    1}/${date.getDate()}/${date.getFullYear()} ${hour}:${minutes}`;
+}
+
 class EthChart extends Component {
   configureChart(arr = []) {
     return {
-      labels: arr.map(dataObj => dataObj.time),
+      labels: arr.map(dataObj => convertDate(dataObj.time)),
       datasets: [
         {
           label: '',
@@ -20,27 +28,34 @@ class EthChart extends Component {
     var arrayToDisplay;
     switch (category) {
       case today:
+        // Show currency within last 24 hours
         return arr.slice().reverse().slice(0, 24).reverse();
 
       case week:
-        slicedArr = arr.slice().reverse().slice(0, 168);
+        slicedArr = arr.slice().reverse().slice(0, 168); // 168 hours in 1 week
         var arrayToDisplay = [];
+
+        // Show highest currency from each day of the week
         for (let i = 0; i < slicedArr.length; i += 24) {
           arrayToDisplay.push(slicedArr[i]);
         }
         return arrayToDisplay.reverse();
 
       case month:
-        var slicedArr = arr.slice().reverse().slice(0, 730);
+        var slicedArr = arr.slice().reverse().slice(0, 730); // 730 hours in 1 month
         var arrayToDisplay = [];
+
+        // Show highest currency from each day in a month
         for (let i = 0; i < slicedArr.length; i += 24) {
           arrayToDisplay.push(slicedArr[i]);
         }
         return arrayToDisplay.reverse();
 
       case year:
-        var slicedArr = arr.slice().reverse().slice(0, 8760);
+        var slicedArr = arr.slice().reverse().slice(0, 8760); // 8760 hours in 1 year
         var arrayToDisplay = [];
+
+        // Show highest currency from each month in a year
         for (let i = 0; i < slicedArr.length; i += 730) {
           arrayToDisplay.push(slicedArr[i]);
         }
@@ -55,7 +70,7 @@ class EthChart extends Component {
       this.props.ethData,
       this.props.category
     );
-
+    console.log(this.props);
     return (
       <div>
         <Line data={this.configureChart(modifiedData)} />
